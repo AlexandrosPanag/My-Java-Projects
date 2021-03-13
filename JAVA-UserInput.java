a32d19b48d4e8da98c20c9ab5ad5c8cac1f4db42
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-03-13 11:32:14 +0200

    Add files via upload

---
+++ JAVA-UserInput.java
@@ -0,0 +1,14 @@
+--------------User Input----------------------------------------------------------------------
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+   int myage;
+   myage=Integer.parseInt(
+   JOptionPane.showInputDialog("Please give your age"));
+   System.out.println("Your age is:"+myage);
+   //Alternative version
+   //JOptionPane.showMessageDialog(null,"Your age is:"+myage);
+   }
+    
+}
